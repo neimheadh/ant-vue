b3ccf3a3cb7555ccc2698ae6fa64d6b03c88efac
Mathieu Wambre <contact@neimheadh.fr>
2021-09-20 16:11:06 +0200

    [IndexedDB] Remove default content

--- database/tables/Transaction.ts
+++ database/tables/Transaction.ts
@@ -26,16 +26,6 @@ export default class Transaction implements IDatabaseTable {
     /**
      * {@inheritdoc}
      */
-    readonly default_content = [
-        { uuid: '7c4de92d-350e-4211-8b5c-71b5564847dc', account: '64ce919a-3c29-4568-a8dd-0a3ecd4d1acd', label: 'Salary', balance: 2000 },
-        { uuid: '9a272eb0-b6b9-4a07-a272-fec7aa95f8cb', account: '64ce919a-3c29-4568-a8dd-0a3ecd4d1acd', label: 'Burger', balance: -34.8 },
-        { uuid: 'bdbba740-2b5f-497e-9841-91b072fb14c3', account: '64ce919a-3c29-4568-a8dd-0a3ecd4d1acd', label: 'Month saving', balance: -500 },
-        { uuid: 'c4bf10ae-efed-4c57-939f-0ab62d90c9dc', account: '1ccf2f29-7a3f-4bf5-9d4f-a36f19d28759', label: 'Month saving', balance: 500 },
-    ];
-
-    /**
-     * {@inheritdoc}
-     */
     readonly default_sort = 'operation_date';
 
     /**
